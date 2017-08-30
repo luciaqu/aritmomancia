f0cf701ee31426524cb614b152a340fc69b7bbd9
LuciaQu <luciaquadrinidcv@gmail.com>
2017-08-30 15:45:56 -0300

    Primer commit

---
+++ aritmomancia.js
@@ -0,0 +1,30 @@
+var nombre = process.argv;
+var caracter = 0;
+var amor;
+var social;
+
+function numeroC(nombre){
+	for(i=0, nombre.lenght < i, i++){
+		if (i == a || i == j || i == s){
+			caracter+1;
+		} else if (i == "b" || i == "k" || i == "t"){
+			caracter+2;
+		} else if (i == "c" || i == "l" || i == "u"){
+			caracter+3;
+		} else if (i == "d" || i == "m" || i == "v"){
+			caracter+4;
+		} else if (i == "e" || i == "n" || i == "w"){
+			caracter+5;
+		} else if (i == "f" || i == "o" || i == "x"){
+			caracter+6;
+		} else if (i == "g" || i == "p" || i == "y"){
+			caracter+7;
+		} else if (i == "h" || i == "q" || i == "z"){
+			caracter+8;
+		} else if (i == "i" || i == "r"){
+			caracter+9;
+		}
+	}
+}
+console.log("ingrese su nombre");
+console.log (caracter);
