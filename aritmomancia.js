5cdcad24052e2b2f7ff042a75174a416e43bc249
LuciaQu <luciaquadrinidcv@gmail.com>
2017-08-30 18:52:08 -0300

    primera suma de todos los números

--- aritmomancia.js
+++ aritmomancia.js
@@ -1,9 +1,12 @@
 
 var amor = 0 ;
-var social=;
+var social= 0;
 var nombre = ["s","o","f","i","a","g","u","t","i","e","r","r","e","z"];
 var caracter= 0;
 var letrasArray = [];
+var letraAmor = [];
+var letraSocial = [];
+
 	
 function leer (){ 
 	for(var i=0; i < nombre.length; i++){
@@ -12,61 +15,82 @@ function leer (){
 			letrasArray+= nombre[i];
 			if(nombre[i]=="a"){
 				amor+=1;
-			} 
+				letraAmor+= nombre[i];
+			} else {
+				social+=1;
+				letraSocial+= nombre[i];
+			}
+
 		} else if (nombre[i] == "b" || nombre[i] == "k" || nombre[i] == "t"){
 			caracter+=2;
 			letrasArray+= nombre[i];
+			social+=2;
+			letraSocial+= nombre[i];
+
 		} else if (nombre[i] == "c" || nombre[i] == "l" || nombre[i] == "u"){
 			caracter+=3;
+			letrasArray+= nombre[i];
 			if(nombre[i]=="u"){
 				amor+=3;
+				letraAmor+= nombre[i];
+			} else {
+				social+=3;
+				letraSocial+= nombre[i];
 			}
-			letrasArray+= nombre[i];
+			
 		} else if (nombre[i] == "d" || nombre[i] == "m" || nombre[i] == "v"){
 			caracter+=4;
 			letrasArray+= nombre[i];
+			social+=4;
+			letraSocial+= nombre[i];
+		
 		} else if (nombre[i] == "e" || nombre[i] == "n" || nombre[i] == "w"){
 			caracter+=5;
 			letrasArray+= nombre[i];
 			if(nombre[i]=="e"){
-				amor+=5
+				amor+=5;
+				letraAmor+= nombre[i];
+			} else {
+				social+=5;
+				letraSocial+= nombre[i];
 			}
+		
 		} else if (nombre[i] == "f" || nombre[i] == "o" || nombre[i] == "x"){
 			caracter+=6;
 			letrasArray+= nombre[i];
 			if(nombre[i]=="o"){
-				amor+=6
+				amor+=6;
+				letraAmor+= nombre[i];
+			} else {
+				social+=6;
+				letraSocial+= nombre[i];
 			}
 		} else if (nombre[i] == "g" || nombre[i] == "p" || nombre[i] == "y"){
 			caracter+=7;
 			letrasArray+= nombre[i];
+			social+=7;
+			letraSocial+= nombre[i];
 		} else if (nombre[i] == "h" || nombre[i] == "q" || nombre[i] == "z"){
 			caracter+=8;
 			letrasArray+= nombre[i];
+			social+=8;
+			letraSocial+= nombre[i];
 		} else if (nombre[i] == "i" || nombre[i] == "r"){
 			caracter+=9;
 			letrasArray+= nombre[i];
 			if(nombre[i]=="i"){
-				amor+=9
+				amor+=9;
+				letraAmor+= nombre[i];
+			}else {
+				social+=9;
+				letraSocial+= nombre[i];
+
 			}
 		}
 	}
 	console.log("caracter ="+ caracter+ " letrasArray= "+letrasArray);
-
-	/*for(var i=0; i < nombre.length; i++){
-		if (nombre[i] == "a"){
-			amor+=1;
-		} else if (nombre[i] == "u"){
-			amor+=3;
-		} else if (nombre[i] == "e"){
-			amor+=5;
-		} else if (nombre[i] == "o"){
-			amor+=6;
-		} else if (nombre[i] == "i"){
-			amor+=9;
-		}
-	}*/
-	console.log("amor ="+ amor);
+	console.log("amor ="+ amor+ " letras= " + letraAmor);
+	console.log("social ="+ social+ " letras= "+ letraSocial);
 
 }
 leer(nombre);
