5ea2da230ab929ae111ae9ce09daec60d26db85b
LuciaQu <luciaquadrinidcv@gmail.com>
2017-08-30 18:34:49 -0300

    primer suma funciona, nro amor también

--- aritmomancia.js
+++ aritmomancia.js
@@ -1,32 +1,72 @@
 
-var amor;
-var social;
-var nombre = [process.argv];
+var amor = 0 ;
+var social=;
+var nombre = ["s","o","f","i","a","g","u","t","i","e","r","r","e","z"];
 var caracter= 0;
+var letrasArray = [];
 	
 function leer (){ 
-	for(var i=0; nombre.length < i; i++){
+	for(var i=0; i < nombre.length; i++){
 		if (nombre[i] == "a"|| nombre[i] == "j" || nombre[i] == "s"){
 			caracter+=1;
+			letrasArray+= nombre[i];
+			if(nombre[i]=="a"){
+				amor+=1;
+			} 
 		} else if (nombre[i] == "b" || nombre[i] == "k" || nombre[i] == "t"){
 			caracter+=2;
+			letrasArray+= nombre[i];
 		} else if (nombre[i] == "c" || nombre[i] == "l" || nombre[i] == "u"){
 			caracter+=3;
+			if(nombre[i]=="u"){
+				amor+=3;
+			}
+			letrasArray+= nombre[i];
 		} else if (nombre[i] == "d" || nombre[i] == "m" || nombre[i] == "v"){
 			caracter+=4;
+			letrasArray+= nombre[i];
 		} else if (nombre[i] == "e" || nombre[i] == "n" || nombre[i] == "w"){
 			caracter+=5;
+			letrasArray+= nombre[i];
+			if(nombre[i]=="e"){
+				amor+=5
+			}
 		} else if (nombre[i] == "f" || nombre[i] == "o" || nombre[i] == "x"){
 			caracter+=6;
+			letrasArray+= nombre[i];
+			if(nombre[i]=="o"){
+				amor+=6
+			}
 		} else if (nombre[i] == "g" || nombre[i] == "p" || nombre[i] == "y"){
 			caracter+=7;
+			letrasArray+= nombre[i];
 		} else if (nombre[i] == "h" || nombre[i] == "q" || nombre[i] == "z"){
 			caracter+=8;
+			letrasArray+= nombre[i];
 		} else if (nombre[i] == "i" || nombre[i] == "r"){
 			caracter+=9;
+			letrasArray+= nombre[i];
+			if(nombre[i]=="i"){
+				amor+=9
+			}
 		}
 	}
-	console.log("caracter ="+ caracter);
+	console.log("caracter ="+ caracter+ " letrasArray= "+letrasArray);
+
+	/*for(var i=0; i < nombre.length; i++){
+		if (nombre[i] == "a"){
+			amor+=1;
+		} else if (nombre[i] == "u"){
+			amor+=3;
+		} else if (nombre[i] == "e"){
+			amor+=5;
+		} else if (nombre[i] == "o"){
+			amor+=6;
+		} else if (nombre[i] == "i"){
+			amor+=9;
+		}
+	}*/
+	console.log("amor ="+ amor);
 
 }
 leer(nombre);
